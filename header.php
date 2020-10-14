b944b9c8f3abfbf5491098c9d37b2352a7c4a3ba
Robbitmm <46769452+Robbitmm@users.noreply.github.com>
2020-10-14 16:40:09 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,32 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+
+        <meta name="viewport" content="width=device-width, inicial-scale=1.0">
+        <script src="scripts/Btn.js"></script>
+
+        <link rel="stylesheet" href="styles/header.css"/>
+        <link rel="stylesheet" href="styles/page.css"/>
+
+        <title>Home - Watchers</title>
+    </head>
+
+    <body>
+        <header>
+            <nav>
+                <ul id="Btn" class="Menu Menu_bar close">
+                    <li><a href="#">Menu</a></li>
+                    <!--<li><a href="#">Categorias</a></li>-->
+                    <li><a href="#Filmes">Filmes</a></li>
+                    <li><a href="#Series">Séries</a></li>
+                </ul>
+            </nav>
+    
+            <img id="BtnMenu" class="btnMenu open" src="images/header/Btn_menu.png" onclick="Condition_menu()"/>
+            <img id="logo" class="Logo" src="images/header/Logo black.png"/>
+            <img class="search_icon" src="images/header/Search.png" onclick="Condition_search()"/>
+    
+            <input id="search_input" class="search close"/>
+        </header>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, inicial-scale=1.0">
+        <link rel="icon" href="images\header\Logo.png" type="image/png type">
+
+    </head>
+
+    <body>
+        <?php
+            require_once("header.php");
+        ?>
+
+        <div id="content">
+
+        </div>
+    </body>
+</html>
