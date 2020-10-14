28dac410ba09905608a4bfc33895844a683c9f9a
Robbitmm <46769452+Robbitmm@users.noreply.github.com>
2020-10-14 17:54:05 -0300

    Add files via upload

--- header.php
+++ header.php
@@ -1,7 +1,5 @@
 <html lang="pt-br">
     <head>
-        <meta charset="utf-8"/>
-
         <meta name="viewport" content="width=device-width, inicial-scale=1.0">
         <script src="scripts/Btn.js"></script>
 
--- index.php
+++ index.php
@@ -3,16 +3,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, inicial-scale=1.0">
         <link rel="icon" href="images\header\Logo.png" type="image/png type">
-
     </head>
 
     <body>
         <?php
-            include_once("header.php");
+            include_once('header.php');
         ?>
-
-        <div id="content">
-
-        </div>
     </body>
 </html>
